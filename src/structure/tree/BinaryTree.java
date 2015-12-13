38712ffb0c467773d8d3e7fd7e5e3a4c1efa22e6
Jameslily <xuwuji@192.168.1.100>
2015-12-13 21:33:28 +0800

    12-13

--- src/structure/tree/BinaryTree.java
+++ src/structure/tree/BinaryTree.java
@@ -132,6 +132,11 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		return pointer;
 	}
 
+	/**
+	 * in-order:left,root,right
+	 * 
+	 * @param pointer
+	 */
 	public void inOrderTraverse(Node pointer) {
 		if (pointer != null) {
 			inOrderTraverse(pointer.left);
@@ -140,6 +145,11 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		}
 	}
 
+	/**
+	 * pre-order:root, left, right
+	 * 
+	 * @param pointer
+	 */
 	public void PreOrderTraverse(Node pointer) {
 		if (pointer != null) {
 			System.out.println(pointer.key);
@@ -148,6 +158,11 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		}
 	}
 
+	/**
+	 * post-order:left,right,root
+	 * 
+	 * @param pointer
+	 */
 	public void PostOrderTraverse(Node pointer) {
 		if (pointer != null) {
 			PostOrderTraverse(pointer.left);
@@ -156,6 +171,11 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		}
 	}
 
+	/**
+	 * level-order:up to down, left to right
+	 * 
+	 * @param pointer
+	 */
 	public void LevelOrderTraverse(Node pointer) {
 		if (queue.isEmpty()) {
 			queue.enque(pointer);
@@ -175,6 +195,11 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 
 	}
 
+	/**
+	 * reverse-level-order:up to down, right to left
+	 * 
+	 * @param pointer
+	 */
 	public void ReverseLevelOrderTraverse(Node pointer) {
 		if (queue.isEmpty()) {
 			queue.enque(pointer);
@@ -192,6 +217,93 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 			}
 		}
 
+	}
+
+	/**
+	 * count the all nodes under a node
+	 * 
+	 * @param pointer
+	 * @return
+	 */
+	public int getNodesCount(Node pointer) {
+		if (pointer == null) {
+			return 0;
+		} else {
+			return 1 + getNodesCount(pointer.left) + getNodesCount(pointer.right);
+		}
+	}
+
+	/**
+	 * get the maximum height from a node to its leaf nodes. A node without
+	 * leaves should be 0 height,the root should be begin from 0
+	 * 
+	 * @param pointer
+	 * @return
+	 */
+	public int getMaxHeight(Node pointer) {
+		if (pointer == null) {
+			return -1;
+		}
+
+		return 1 + Math.max(getMaxHeight(pointer.left), getMaxHeight(pointer.right));
+	}
+
+	/**
+	 * get the minimum height from a node to its leaf nodesA node without leaves
+	 * should be 0 height,the root should be begin from 0
+	 * 
+	 * @param pointer
+	 * @return
+	 */
+	public int getMinHeight(Node pointer) {
+		if (pointer == null) {
+			return -1;
+		}
+
+		if (pointer.left == null) {
+			return 1 + getMinHeight(pointer.right);
+		}
+
+		if (pointer.right == null) {
+			return 1 + getMinHeight(pointer.left);
+		}
+
+		return 1 + Math.min(getMinHeight(pointer.left), getMinHeight(pointer.right));
+	}
+
+	/**
+	 * Get all nodes on a given level. The tree begins from level 0
+	 * 
+	 * @param k
+	 * @return
+	 */
+	public int getLevelNodesCount(int k) {
+		return getLevelNodesCounthelper(root, k);
+	}
+
+	/**
+	 * The helper method.
+	 * 
+	 * The k can be seen as how many levels are checking for the pointer.
+	 * 
+	 * For example, (root,2) represents check the nodes under root in two
+	 * levels. If the pointer is null,the it is a null node, then it should
+	 * return 0. If the k=0, it means it is already on the checking level, so it
+	 * should return 0.
+	 * 
+	 * @param pointer
+	 * @param k
+	 * @return
+	 */
+	public int getLevelNodesCounthelper(Node pointer, int k) {
+		if (pointer == null || k < 0) {
+			return 0;
+		} else if (k == 0) {
+			return 1;
+		} else {
+			return getLevelNodesCounthelper(pointer.left, k - 1) + getLevelNodesCounthelper(pointer.right, k - 1);
+
+		}
 	}
 
 	public boolean isBST(Node pointer) {
@@ -213,18 +325,6 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		return true;
 	}
 
-	public int getMaxHeight(Node pointer) {
-		if (pointer == null)
-			return -1;
-		return 1 + Math.max(getMaxHeight(pointer.left), getMaxHeight(pointer.right));
-	}
-
-	public int getMinHeight(Node pointer) {
-		if (pointer == null)
-			return -1;
-		return 1 + Math.min(getMinHeight(pointer.left), getMinHeight(pointer.right));
-	}
-
 	public static void main(String[] args) {
 		BinaryTree<Integer, String> tree = new BinaryTree<Integer, String>();
 		tree.addNode(6, "f");
@@ -238,22 +338,27 @@ public class BinaryTree<Key extends Comparable<Key>, E> {
 		tree.addNode(2, "b");
 		tree.addNode(5, "e");
 		tree.addNode(9, "i");
+		tree.addNode(10, "j");
 		// tree.delete(9, tree.root);
-		tree.delete(3);
+		// tree.delete(3);
 		// tree.root.left.left = tree.root.left.left.right;
 		// tree.deleteMin(tree.root);
 		// tree.deleteMin(tree.root);
 		// tree.inOrderTraverse(tree.root);
 		// tree.PreOrderTraverse(tree.root);
 		// tree.PostOrderTraverse(tree.root);
-		tree.LevelOrderTraverse(tree.root);
+		// tree.LevelOrderTraverse(tree.root);
 		// tree.ReverseLevelOrderTraverse(tree.root);
 
-		System.out.println(tree.find(6));
-		System.out.println(tree.root.toString());
-		System.out.println(tree.isBST(tree.root));
-		System.out.println(tree.getMaxHeight(tree.root));
-		System.out.println(tree.getMinHeight(tree.root));
+		// System.out.println(tree.find(6));
+		// System.out.println(tree.root.toString());
+		// System.out.println(tree.isBST(tree.root));
+		System.out.println("max:" + tree.getMaxHeight(tree.root));
+		System.out.println("min:" + tree.getMinHeight(tree.root));
+		System.out.println(tree.getNodesCount(tree.root));
+		int k = 2;
+		System.out.println("the nodes on the " + k + " level:" + tree.getLevelNodesCount(k));
+		System.out.println(tree.getLevelNodesCounthelper(null, 2));
 	}
 
 }
